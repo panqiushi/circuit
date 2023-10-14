007f0e36a51159dcc8c8bbaec552de12692fc373
Pan Qiushi <pqs07@qq.com>
2023-10-14 19:02:38 +0800

    signup back to login

--- internal/web/login_page.go
+++ internal/web/login_page.go
@@ -25,13 +25,14 @@ func RegisterLoginPageRoutes(router *gin.Engine) {
 	router.GET("/signup", func(context *gin.Context) {
 		localizer := context.MustGet("localizer").(*i18n.Localizer)
 		context.HTML(http.StatusOK, "signup.html", gin.H{
-			"Title":    localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "sign_up"}),
-			"SignIn":   localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "sign_in"}),
-			"SignUp":   localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "sign_up"}),
-			"Welcome":  localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "welcome"}),
-			"Or":       localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "or"}),
-			"Email":    localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "email"}),
-			"Password": localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "password"}),
+			"Title":             localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "sign_up"}),
+			"SignIn":            localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "sign_in"}),
+			"SignUp":            localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "sign_up"}),
+			"Welcome":           localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "welcome"}),
+			"Or":                localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "or"}),
+			"Email":             localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "email"}),
+			"Password":          localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "password"}),
+			"AlreadHaveAccount": localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: "already_have_account"}),
 		})
 	})
 
