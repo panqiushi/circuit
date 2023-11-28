e80f0c76aaf5c2ef6bfdc4ec683a17f23a524787
Asimov <pqs07@qq.com>
2023-11-28 13:43:57 +0000

    done create version release form

--- internal/models/version_release_model.go
+++ internal/models/version_release_model.go
@@ -7,8 +7,8 @@ type VersionRelease struct {
 	ID          uint   `gorm:"primary_key;auto_increment;column:id" json:"id"`
 	VersionName string `gorm:"unique;column:version_name" json:"version_name" form:"version_name"`
 	ReleaseDate string `gorm:"column:release_date" json:"release_date" form:"release_date"`
-	ProjectID   uint   `gorm:"foreignkey:ProjectID;column:project_id" json:"project_id"`
-	CommitID    string `gorm:"unique;column:commit_id" json:"commit_id"`
+	ProjectID   uint   `gorm:"foreignkey:ProjectID;column:project_id" json:"project_id" form:"project_id"`
+	CommitID    string `gorm:"unique;column:commit_id" json:"commit_id" form:"commit_id"`
 	Branch      string `gorm:"column:branch" json:"branch" form:"branch"`
 	TagName     string `gorm:"unique;column:tag_name" json:"tag_name" form:"tag_name"`
 	UserId      uint   `gorm:"foreignkey:UserId;column:user_id" json:"user_id"`
